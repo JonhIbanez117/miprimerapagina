d9fd6682eeaa0fc971b389614d91df9a6bf40083
JonhIbanez117 <johnibanez343@gmail.com>
2023-10-12 19:45:34 -0500

    Initial commit

---
+++ Imagenes/scrip.js
@@ -0,0 +1,42 @@
+alert("Hola y bienvenido a mi web");
+
+let nombre = "Martha";
+nombre = "Sofia"
+
+var nombre117 = "John";
+
+const nombreB312 = "Andrés";
+
+console.log("Nombres:");
+console.log(nombre);
+
+console.log(nombre117);
+
+console.log(nombreB312);
+
+<form>
+
+<label for="nombre">Nombre:</label> 
+<input type="text" id="nombre" name="nombre"></input>
+
+<label for="email">Correo electrónico:</label>
+
+<input type="email" id="email" name="email"></input>
+
+<label for="mensaje">Mensaje:</label> 
+<textarea id="mensaje" name= "mensaje"></textarea>
+
+ <label for="pais">País: </label>
+
+<select id="pais" name="pais">
+
+<option value="argentina">Argentina</option>
+
+<option value="chile">Chile</option>
+<option value="Colombia">Colombia</option>
+<option value="mexico">México</option> 
+<option value="peru">Perú</option> </select>
+
+<button type="submit">Enviar</button>
+
+</form>
